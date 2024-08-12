1413563736056314b06ea64cb6de9cb73144f2dc
unknown <buivanluu1997@gmail.com>
2024-08-12 08:52:32 +0700

    nop_bai_tap

---
+++ ss7/thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the height');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let acreage = width * height ;
+document.write('the acreage is : '+ acreage);
