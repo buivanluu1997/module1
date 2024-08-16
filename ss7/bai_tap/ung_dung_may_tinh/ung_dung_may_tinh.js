200576f4b67f1cbff43708f82f3a38bbaa13e6c3
unknown <buivanluu1997@gmail.com>
2024-08-16 09:35:37 +0700

    nop_bai_tap

--- ss7/bai_tap/ung_dung_may_tinh/ung_dung_may_tinh.js
+++ ss7/bai_tap/ung_dung_may_tinh/ung_dung_may_tinh.js
@@ -2,27 +2,27 @@ function cong() {
     let so1 = +document.getElementById('so1').value;
     let so2 = +document.getElementById('so2').value;
     let ketqua;
-    ketqua = Number(so1)+Number(so2)
+    ketqua = Number(so1)+Number(so2);
     document.getElementById('kq').innerHTML='Kết quả là: '+ketqua;
 }
 function tru() {
     let so1 = +document.getElementById('so1').value;
     let so2 = +document.getElementById('so2').value;
     let ketqua;
-    ketqua = Number(so1)-Number(so2)
+    ketqua = Number(so1)-Number(so2);
     document.getElementById('kq').innerHTML='Kết quả là: '+ketqua;
 }
 function nhan() {
     let so1 = +document.getElementById('so1').value;
     let so2 = +document.getElementById('so2').value;
     let ketqua;
-    ketqua = Number(so1)*Number(so2)
+    ketqua = Number(so1)*Number(so2);
     document.getElementById('kq').innerHTML='Kết quả là: '+ketqua;
 }
 function chia() {
     let so1 = +document.getElementById('so1').value;
     let so2 = +document.getElementById('so2').value;
     let ketqua;
-    ketqua = Number(so1)/Number(so2)
+    ketqua = Number(so1)/Number(so2);
     document.getElementById('kq').innerHTML='Kết quả là: '+ketqua;
 }
--- ss7/thuc_hanh/su_dung_toan_tu.js
+++ ss7/thuc_hanh/su_dung_toan_tu.js
@@ -1,6 +1,6 @@
 let inputWidth;
 let inputHeight;
-inputWidth = prompt('Enter the width');
+inputWidth =prompt('Enter the width');
 inputHeight = prompt('Enter the height');
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
